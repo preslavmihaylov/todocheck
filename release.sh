a66f61e5bf339c0ab11a494766296c5273b6620e
Preslav Mihaylov <preslav@uber.com>
2020-07-18 20:05:28 +0300

    add script for generating release artifacts

---
+++ release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DIR=$1
+VERSION=$2
+
+if [ -z "$DIR" ] || [ -z "$VERSION" ]; then 
+    echo "Usage: release.sh <target-dir> <version>"
+    exit 1
+fi
+
+function create_build {
+    GOOS=$1
+    GOARCH=$2
+    EXT=$3
+    if [ -z $EXT ]; then
+        EXT=$GOARCH
+    fi
+
+    BINARY=todocheck-$VERSION-$GOOS-$EXT
+    go build -o $DIR/$BINARY
+    cd $DIR && shasum -a 256 $BINARY > $BINARY.sha256 && cd ..
+}
+
+mkdir -p $DIR
+create_build darwin amd64 x86_64
+create_build linux amd64 x86_64
+create_build linux arm64
+create_build windows amd64 x86_64
