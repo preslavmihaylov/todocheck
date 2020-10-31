59dbe273892046c967821a03107707b962d47430
Preslav Mihaylov <preslav@uber.com>
2020-10-31 15:31:47 +0200

    renamed all occurrencces of redmine to pivotaltracker in the pivotaltracker component

--- issuetracker/pivotaltracker/pivotaltracker.go
+++ issuetracker/pivotaltracker/pivotaltracker.go
@@ -8,17 +8,17 @@ import (
 	"github.com/preslavmihaylov/todocheck/issuetracker"
 )
 
-// IssueTracker implementation for integrating with public & private redmine issue trackers
+// IssueTracker implementation for integrating with public pivotaltracker issue trackers
 type IssueTracker struct {
 	Origin string
 }
 
-// TaskModel returns the model representing a deserialized redmine task
+// TaskModel returns the model representing a deserialized pivotaltracker task
 func (it *IssueTracker) TaskModel() issuetracker.Task {
 	return &Task{}
 }
 
-// TaskURLFrom taskID returns the url for the target redmine task ID to fetch
+// TaskURLFrom taskID returns the url for the target pivotaltracker task ID to fetch
 func (it *IssueTracker) TaskURLFrom(taskID string) string {
 	if len(taskID) > 0 && taskID[0] == '#' {
 		return taskID[1:]
@@ -27,7 +27,7 @@ func (it *IssueTracker) TaskURLFrom(taskID string) string {
 	return taskID
 }
 
-// IssueAPIOrigin returns the URL for redmine's issue-fetching API
+// IssueAPIOrigin returns the URL for pivotaltracker's issue-fetching API
 func (it *IssueTracker) IssueAPIOrigin() string {
 	tokens := common.RemoveEmptyTokens(strings.Split(it.Origin, "/"))
 	if tokens[0] == "pivotaltracker.com" {
