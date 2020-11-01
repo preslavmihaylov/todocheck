preslavmihaylov/todocheck

commit b9b3e4326544ea161417ce2d1c8e3212e3eb4330

unblock issues caused by bad validation (#85)
* unblock issues caused by bad validation

* remove commented out code

main.go
@@ -42,17 +42,17 @@ func main() {
 		log.Fatalf("couldn't open configuration file: %s\n", err)
 	}
 
+	err = authmanager.AcquireToken(localCfg)
+	if err != nil {
+		log.Fatalf("couldn't acquire token from config: %s\n", err)
+	}
+
 	if errors := validation.Validate(localCfg); len(errors) > 0 {
 		for _, err := range errors {
 			log.Println(err)
 		}
 
 		os.Exit(1)
-	}
-
-	err = authmanager.AcquireToken(localCfg)
-	if err != nil {
-		log.Fatalf("couldn't acquire token from config: %s\n", err)
 	}
 
 	baseURL, err := issuetracker.BaseURLFor(localCfg.IssueTracker, localCfg.Origin)

validation/validation.go
@@ -27,10 +27,6 @@ func Validate(cfg *config.Local) []error {
 		errs = append(errs, err)
 	}
 
-	if err := validateIssueTrackerExists(cfg); err != nil {
-		errs = append(errs, err)
-	}
-
 	if err := validateAuthType(cfg); err != nil {
 		errs = append(errs, err)
 	}

commit 495a2181a0264e50f844f4d641046c502362ce8a

refactored issue tracker component, extracting specific issue tracker code into separate components (#81)
* refactored issue tracker component, applying dependency inversion

* fix failing build for tests

* renamed all occurrencces of redmine to pivotaltracker in the pivotaltracker component

* fix issue #71 (#84)

Co-authored-by: zen <zen@zen>

* unblock issues caused by bad validation (#85)

* unblock issues caused by bad validation

* remove commented out code

* Enable annotating issues with hashtag (#86)

* Enable annotating github issues with hashtag

* add test

* revert ParseGithubDetails to parseGithubDetails

* revert README and auto_detect_config's change

* Add test for hashtag todos with github

* Add ExpectJSONOutput (#77)

* Add ExpectJSONOutput

* rename ExpectJsonOutput to WithJSONOutput, divide validateTodoErrs into two funcs

* modify the way to compare json todo errs and scenarios

* Add TestAnnotatedTodosWithJSONOutput

Co-authored-by: Preslav Mihaylov <preslav@uber.com>

* re-fixes issue #71 broken by PR #85 (#87)

* re-fixes issue #71 broken by PR #85

* re-fixes issue #71, removes validation of auth type from validation.go

Co-authored-by: zen <zen@zen>
Co-authored-by: Preslav Mihaylov <preslav@uber.com>

* refactored issue tracker component, applying dependency inversion

Co-authored-by: zen37 <mobi2009ro@gmail.com>
Co-authored-by: zen <zen@zen>
Co-authored-by: liao <liao_shizhen@yahoo.co.jp>

fetcher/fetcher.go
@@ -14,19 +14,18 @@ import (
 
 // Fetcher for task statuses by contacting task management web apps' rest api
 type Fetcher struct {
-	origin         string
-	tracker        config.IssueTracker
+	issuetracker.IssueTracker
 	authMiddleware authmiddleware.Func
 }
 
 // NewFetcher instance
-func NewFetcher(origin string, tracker config.IssueTracker, authMw authmiddleware.Func) *Fetcher {
-	return &Fetcher{origin, tracker, authMw}
+func NewFetcher(issueTracker issuetracker.IssueTracker, authMw authmiddleware.Func) *Fetcher {
+	return &Fetcher{issueTracker, authMw}
 }
 
 // Fetch a task's status based on task ID
 func (f *Fetcher) Fetch(taskID string) (taskstatus.TaskStatus, error) {
-	req, err := http.NewRequest("GET", f.origin+issuetracker.TaskURLSuffixFor(taskID, f.tracker), nil)
+	req, err := http.NewRequest("GET", f.IssueTracker.IssueAPIOrigin()+f.IssueTracker.TaskURLFrom(taskID), nil)
 	if err != nil {
 		return taskstatus.None, fmt.Errorf("failed creating new GET request: %w", err)
 	}
@@ -49,7 +48,7 @@ func (f *Fetcher) Fetch(taskID string) (taskstatus.TaskStatus, error) {
 		return taskstatus.None, fmt.Errorf("bad status code upon fetching task: %d - %s", resp.StatusCode, string(body))
 	}
 
-	task := issuetracker.TaskFor(f.tracker)
+	task := f.IssueTracker.TaskModel()
 	err = json.Unmarshal(body, &task)
 	if err != nil {
 		return taskstatus.None, fmt.Errorf("couldn't unmarshal response task JSON: %w", err)

issuetracker/factory/factory.go
@@ -1,0 +1,32 @@
+package factory
+
+import (
+	"errors"
+
+	"github.com/preslavmihaylov/todocheck/config"
+	"github.com/preslavmihaylov/todocheck/issuetracker"
+	"github.com/preslavmihaylov/todocheck/issuetracker/github"
+	"github.com/preslavmihaylov/todocheck/issuetracker/gitlab"
+	"github.com/preslavmihaylov/todocheck/issuetracker/jira"
+	"github.com/preslavmihaylov/todocheck/issuetracker/pivotaltracker"
+	"github.com/preslavmihaylov/todocheck/issuetracker/redmine"
+)
+
+// NewIssueTrackerFrom is a static factory method for creating an issuetracker.IssueTracker instance based on the chosen issue tracker type
+// in the configuration
+func NewIssueTrackerFrom(issueTrackerType config.IssueTracker, origin string) (issuetracker.IssueTracker, error) {
+	switch issueTrackerType {
+	case config.IssueTrackerGithub:
+		return &github.IssueTracker{Origin: origin}, nil
+	case config.IssueTrackerJira:
+		return &jira.IssueTracker{Origin: origin}, nil
+	case config.IssueTrackerGitlab:
+		return &gitlab.IssueTracker{Origin: origin}, nil
+	case config.IssueTrackerRedmine:
+		return &redmine.IssueTracker{Origin: origin}, nil
+	case config.IssueTrackerPivotal:
+		return &pivotaltracker.IssueTracker{Origin: origin}, nil
+	}
+
+	return nil, errors.New("unknown issue tracker " + string(issueTrackerType))
+}

issuetracker/github/github.go
@@ -1,0 +1,39 @@
+package github
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/preslavmihaylov/todocheck/common"
+	"github.com/preslavmihaylov/todocheck/issuetracker"
+)
+
+// IssueTracker implementation for integrating with public & private github issue trackers
+type IssueTracker struct {
+	Origin string
+}
+
+// TaskModel returns the model representing a deserialized github task
+func (it *IssueTracker) TaskModel() issuetracker.Task {
+	return &Task{}
+}
+
+// TaskURLFrom taskID returns the url for the target github task ID to fetch
+func (it *IssueTracker) TaskURLFrom(taskID string) string {
+	if len(taskID) > 0 && taskID[0] == '#' {
+		return taskID[1:]
+	}
+
+	return taskID
+}
+
+// IssueAPIOrigin returns the URL for github's issue-fetching API
+func (it *IssueTracker) IssueAPIOrigin() string {
+	tokens := common.RemoveEmptyTokens(strings.Split(it.Origin, "/"))
+	if !strings.HasPrefix(tokens[0], "http") {
+		tokens = append([]string{"https:"}, tokens...)
+	}
+
+	scheme, owner, repo := tokens[0], tokens[2], tokens[3]
+	return fmt.Sprintf("%s//api.github.com/repos/%s/%s/issues/", scheme, owner, repo)
+}

issuetracker/github/task.go
@@ -1,14 +1,14 @@
-package models
+package github
 
 import "github.com/preslavmihaylov/todocheck/issuetracker/taskstatus"
 
-// GitlabTask model
-type GitlabTask struct {
+// Task model
+type Task struct {
 	State string `json:"state"`
 }
 
-// GetStatus of gitlab task, based on underlying structure
-func (t *GitlabTask) GetStatus() taskstatus.TaskStatus {
+// GetStatus of github task, based on underlying structure
+func (t *Task) GetStatus() taskstatus.TaskStatus {
 	switch t.State {
 	case "closed":
 		return taskstatus.Closed

issuetracker/gitlab/gitlab.go
@@ -1,0 +1,37 @@
+package gitlab
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/preslavmihaylov/todocheck/common"
+	"github.com/preslavmihaylov/todocheck/issuetracker"
+)
+
+// IssueTracker is an issue tracker implementation for integrating with public & private gitlab issue trackers
+type IssueTracker struct {
+	Origin string
+}
+
+// TaskModel returns the model representing a deserialized gitlab task
+func (it *IssueTracker) TaskModel() issuetracker.Task {
+	return &Task{}
+}
+
+// TaskURLFrom taskID returns the url for the target gitlab task ID to fetch
+func (it *IssueTracker) TaskURLFrom(taskID string) string {
+	return taskID
+}
+
+// IssueAPIOrigin returns the URL for github's issue-fetching API
+func (it *IssueTracker) IssueAPIOrigin() string {
+	tokens := common.RemoveEmptyTokens(strings.Split(it.Origin, "/"))
+	if tokens[0] != "http:" && tokens[0] != "https:" {
+		tokens = append([]string{"https:"}, tokens...)
+	}
+
+	scheme, host, owner, repo := tokens[0], tokens[1], tokens[2], tokens[3]
+	urlEncodedProject := url.QueryEscape(fmt.Sprintf("%s/%s", owner, repo))
+	return fmt.Sprintf("%s//%s/api/v4/projects/%s/issues/", scheme, host, urlEncodedProject)
+}

issuetracker/gitlab/task.go
@@ -1,14 +1,14 @@
-package models
+package gitlab
 
 import "github.com/preslavmihaylov/todocheck/issuetracker/taskstatus"
 
-// GithubTask model
-type GithubTask struct {
+// Task model for gitlab tasks
+type Task struct {
 	State string `json:"state"`
 }
 
-// GetStatus of jira task, based on underlying structure
-func (t *GithubTask) GetStatus() taskstatus.TaskStatus {
+// GetStatus of gitlab task, based on underlying structure
+func (t *Task) GetStatus() taskstatus.TaskStatus {
 	switch t.State {
 	case "closed":
 		return taskstatus.Closed

issuetracker/issuetracker.go
@@ -3,87 +3,36 @@ package issuetracker
 import (
 	"errors"
 	"fmt"
-	"net/url"
 	"strings"
 
 	"github.com/preslavmihaylov/todocheck/common"
 	"github.com/preslavmihaylov/todocheck/config"
-	"github.com/preslavmihaylov/todocheck/issuetracker/models"
+	"github.com/preslavmihaylov/todocheck/issuetracker/taskstatus"
 )
 
 // ErrUnsupportedHealthCheck is returned when the health check doesn't support the given issue tracker
 var ErrUnsupportedHealthCheck = errors.New("unsupported issue tracker for health check")
 
-// TaskFor gets the corresponding task model, based on the issue tracker type
-func TaskFor(issueTracker config.IssueTracker) models.Task {
-	switch issueTracker {
-	case config.IssueTrackerJira:
-		return &models.JiraTask{}
-	case config.IssueTrackerGithub:
-		return &models.GithubTask{}
-	case config.IssueTrackerGitlab:
-		return &models.GitlabTask{}
-	case config.IssueTrackerPivotal:
-		return &models.PivotalTrackerTask{}
-	case config.IssueTrackerRedmine:
-		return &models.RedmineTask{}
-	default:
-		return nil
-	}
+// Task is an interface for generic task operations, decoupled from the specific platform's task structure
+type Task interface {
+	GetStatus() taskstatus.TaskStatus
 }
 
-// TaskURLSuffixFor the given issue tracker. Returns the appropriate task ID to append to rest api request.
-// In most use-cases, the taskID is returned as-is as it is simply appended to the issue tracker URL origin (e.g. issuetracker.com/issues/{taskID}.
-// However, some issue trackers might expect the taskID to be appended in a special format
-func TaskURLSuffixFor(taskID string, issueTracker config.IssueTracker) string {
-	switch issueTracker {
-	case config.IssueTrackerRedmine:
-		return taskID + ".json"
-	case config.IssueTrackerPivotal, config.IssueTrackerGithub:
-		if len(taskID) > 0 && taskID[0] == '#' {
-			return taskID[1:]
-		}
+// IssueTracker is an interface, which all issue tracker integration components adhere to in order to
+// detach the specific issue trackers from the high-level rules for using issue trackers in the system
+type IssueTracker interface {
+	// returns a Task model, specific to the given issue tracker, which can be unmarshaled from JSON
+	TaskModel() Task
 
-		return taskID
-	default:
-		return taskID
-	}
-}
+	// TaskURLFrom the given task ID. Returns the appropriate task ID to append to rest api request.
+	// In most use-cases, the taskID is returned as-is as it is simply appended to the issue tracker URL origin
+	// (e.g. issuetracker.com/issues/{taskID}.
+	//
+	// However, some issue trackers might expect the taskID to be appended in a special format
+	TaskURLFrom(taskID string) string
 
-// BaseURLFor returns the task-fetching base url given the issue tracker type and the site origin
-func BaseURLFor(issueTracker config.IssueTracker, origin string) (string, error) {
-	switch issueTracker {
-	case config.IssueTrackerJira:
-		return fmt.Sprintf("%s/rest/api/latest/issue/", origin), nil
-	case config.IssueTrackerGithub:
-		scheme, owner, repo := parseGithubDetails(origin)
-		return fmt.Sprintf("%s//api.github.com/repos/%s/%s/issues/", scheme, owner, repo), nil
-	case config.IssueTrackerGitlab:
-		tokens := common.RemoveEmptyTokens(strings.Split(origin, "/"))
-		if tokens[0] != "http:" && tokens[0] != "https:" {
-			tokens = append([]string{"https:"}, tokens...)
-		}
-
-		scheme, host, owner, repo := tokens[0], tokens[1], tokens[2], tokens[3]
-		urlEncodedProject := url.QueryEscape(fmt.Sprintf("%s/%s", owner, repo))
-		return fmt.Sprintf("%s//%s/api/v4/projects/%s/issues/", scheme, host, urlEncodedProject), nil
-	case config.IssueTrackerPivotal:
-		tokens := common.RemoveEmptyTokens(strings.Split(origin, "/"))
-		if tokens[0] == "pivotaltracker.com" {
-			tokens = append([]string{"https:"}, tokens...)
-		}
-
-		scheme, project := tokens[0], tokens[3]
-		if tokens[2] == "n" {
-			project = tokens[4]
-		}
-
-		return fmt.Sprintf("%s//www.pivotaltracker.com/services/v5/projects/%s/stories/", scheme, project), nil
-	case config.IssueTrackerRedmine:
-		return fmt.Sprintf("%s/issues/", origin), nil
-	default:
-		return "", errors.New("unknown issue tracker " + string(issueTracker))
-	}
+	// IssueAPIOrigin returns the task-fetching base url for the given issue tracker
+	IssueAPIOrigin() string
 }
 
 // HealthcheckURL returns the health check base url given the issue tracker type and the site origin

issuetracker/jira/jira.go
@@ -1,0 +1,27 @@
+package jira
+
+import (
+	"fmt"
+
+	"github.com/preslavmihaylov/todocheck/issuetracker"
+)
+
+// IssueTracker is an issue tracker implementation for integrating with private Jira servers
+type IssueTracker struct {
+	Origin string
+}
+
+// TaskModel returns the model representing a deserialized Jira task
+func (it *IssueTracker) TaskModel() issuetracker.Task {
+	return &Task{}
+}
+
+// TaskURLFrom taskID returns the url for the target Jira task ID to fetch
+func (it *IssueTracker) TaskURLFrom(taskID string) string {
+	return taskID
+}
+
+// IssueAPIOrigin returns the URL for Jira's issue-fetching API
+func (it *IssueTracker) IssueAPIOrigin() string {
+	return fmt.Sprintf("%s/rest/api/latest/issue/", it.Origin)
+}

issuetracker/jira/task.go
@@ -1,4 +1,4 @@
-package models
+package jira
 
 import "github.com/preslavmihaylov/todocheck/issuetracker/taskstatus"
 
@@ -12,13 +12,13 @@ type Fields struct {
 	Status `json:"status"`
 }
 
-// JiraTask JSON model as returned by Rest API
-type JiraTask struct {
+// Task JSON model as returned by the Jira Rest API
+type Task struct {
 	Fields `json:"fields"`
 }
 
 // GetStatus of jira task, based on underlying structure
-func (t *JiraTask) GetStatus() taskstatus.TaskStatus {
+func (t *Task) GetStatus() taskstatus.TaskStatus {
 	switch t.Fields.Status.Name {
 	case "Done":
 		fallthrough

issuetracker/models/models.go
@@ -1,8 +1,0 @@
-package models
-
-import "github.com/preslavmihaylov/todocheck/issuetracker/taskstatus"
-
-// Task is an interface for generic task operations, decoupled from the specific platform's task structure
-type Task interface {
-	GetStatus() taskstatus.TaskStatus
-}

issuetracker/pivotaltracker/pivotaltracker.go
@@ -1,0 +1,43 @@
+package pivotaltracker
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/preslavmihaylov/todocheck/common"
+	"github.com/preslavmihaylov/todocheck/issuetracker"
+)
+
+// IssueTracker implementation for integrating with public pivotaltracker issue trackers
+type IssueTracker struct {
+	Origin string
+}
+
+// TaskModel returns the model representing a deserialized pivotaltracker task
+func (it *IssueTracker) TaskModel() issuetracker.Task {
+	return &Task{}
+}
+
+// TaskURLFrom taskID returns the url for the target pivotaltracker task ID to fetch
+func (it *IssueTracker) TaskURLFrom(taskID string) string {
+	if len(taskID) > 0 && taskID[0] == '#' {
+		return taskID[1:]
+	}
+
+	return taskID
+}
+
+// IssueAPIOrigin returns the URL for pivotaltracker's issue-fetching API
+func (it *IssueTracker) IssueAPIOrigin() string {
+	tokens := common.RemoveEmptyTokens(strings.Split(it.Origin, "/"))
+	if tokens[0] == "pivotaltracker.com" {
+		tokens = append([]string{"https:"}, tokens...)
+	}
+
+	scheme, project := tokens[0], tokens[3]
+	if tokens[2] == "n" {
+		project = tokens[4]
+	}
+
+	return fmt.Sprintf("%s//www.pivotaltracker.com/services/v5/projects/%s/stories/", scheme, project)
+}

issuetracker/pivotaltracker/task.go
@@ -1,14 +1,14 @@
-package models
+package pivotaltracker
 
 import "github.com/preslavmihaylov/todocheck/issuetracker/taskstatus"
 
-// PivotalTrackerTask model
-type PivotalTrackerTask struct {
+// Task model
+type Task struct {
 	CurrentState string `json:"current_state"`
 }
 
 // GetStatus of pivotal tracker task, based on underlying structure
-func (t *PivotalTrackerTask) GetStatus() taskstatus.TaskStatus {
+func (t *Task) GetStatus() taskstatus.TaskStatus {
 	switch t.CurrentState {
 	case "finished":
 		fallthrough

issuetracker/redmine/redmine.go
@@ -1,0 +1,27 @@
+package redmine
+
+import (
+	"fmt"
+
+	"github.com/preslavmihaylov/todocheck/issuetracker"
+)
+
+// IssueTracker implementation for integrating with public & private redmine issue trackers
+type IssueTracker struct {
+	Origin string
+}
+
+// TaskModel returns the model representing a deserialized redmine task
+func (it *IssueTracker) TaskModel() issuetracker.Task {
+	return &Task{}
+}
+
+// TaskURLFrom taskID returns the url for the target redmine task ID to fetch
+func (it *IssueTracker) TaskURLFrom(taskID string) string {
+	return taskID + ".json"
+}
+
+// IssueAPIOrigin returns the URL for redmine's issue-fetching API
+func (it *IssueTracker) IssueAPIOrigin() string {
+	return fmt.Sprintf("%s/issues/", it.Origin)
+}

issuetracker/redmine/task.go
@@ -1,9 +1,9 @@
-package models
+package redmine
 
 import "github.com/preslavmihaylov/todocheck/issuetracker/taskstatus"
 
-// RedmineTask model
-type RedmineTask struct {
+// Task model
+type Task struct {
 	Issue struct {
 		Status struct {
 			Name string `json:"name"`
@@ -11,8 +11,8 @@ type RedmineTask struct {
 	} `json:"issue"`
 }
 
-// GetStatus of pivotal tracker task, based on underlying structure
-func (t *RedmineTask) GetStatus() taskstatus.TaskStatus {
+// GetStatus of redmine task, based on underlying structure
+func (t *Task) GetStatus() taskstatus.TaskStatus {
 	switch t.Issue.Status.Name {
 	case "Resolved":
 		fallthrough

main.go
@@ -13,7 +13,7 @@ import (
 	todocheckerrors "github.com/preslavmihaylov/todocheck/checker/errors"
 	"github.com/preslavmihaylov/todocheck/config"
 	"github.com/preslavmihaylov/todocheck/fetcher"
-	"github.com/preslavmihaylov/todocheck/issuetracker"
+	"github.com/preslavmihaylov/todocheck/issuetracker/factory"
 	"github.com/preslavmihaylov/todocheck/traverser/todoerrs"
 	"github.com/preslavmihaylov/todocheck/validation"
 )
@@ -55,14 +55,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	baseURL, err := issuetracker.BaseURLFor(localCfg.IssueTracker, localCfg.Origin)
+	tracker, err := factory.NewIssueTrackerFrom(localCfg.IssueTracker, localCfg.Origin)
 	if err != nil {
-		log.Fatalf("couldn't get base url for origin %s & issue tracker %s: %s\n",
-			localCfg.Origin, localCfg.IssueTracker, err)
+		log.Fatalf("couldn't create new issue tracker: %s\n", err)
 	}
 
+	f := fetcher.NewFetcher(tracker, authmiddleware.For(localCfg))
+
 	todoErrs := []*todocheckerrors.TODO{}
-	f := fetcher.NewFetcher(baseURL, localCfg.IssueTracker, authmiddleware.For(localCfg))
 	traverser := todoerrs.NewTraverser(f, localCfg.IgnoredPaths, func(todoErr *todocheckerrors.TODO) error {
 		todoErrs = append(todoErrs, todoErr)
 		return nil

testing/scenariobuilder/issuetracker/issuetracker.go
@@ -3,7 +3,7 @@ package issuetracker
 import (
 	"encoding/json"
 
-	"github.com/preslavmihaylov/todocheck/issuetracker/models"
+	"github.com/preslavmihaylov/todocheck/issuetracker/jira"
 )
 
 // Type is an enum specifying the target mock issue tracker to use
@@ -41,9 +41,9 @@ func IssueURLFrom(t Type, issue string) string {
 func BuildResponseFor(t Type, issue string, status Status) []byte {
 	switch t {
 	case Jira:
-		return must(json.Marshal(&models.JiraTask{
-			Fields: models.Fields{
-				Status: models.Status{
+		return must(json.Marshal(&jira.Task{
+			Fields: jira.Fields{
+				Status: jira.Status{
 					Name: string(status),
 				},
 			},