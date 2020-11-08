14b683fc82de06d8dc86b1b1344a77293d1782ad
sattellite <null@sattellite.me>
2020-11-08 16:27:40 +0300

    Fixed implementation of methods in logger

--- logger/logger.go
+++ logger/logger.go
@@ -26,25 +26,25 @@ func Setup(verbose bool) {
 }
 
 // Info prints in verbose mode to standart output. Format according to fmt.Println.
-func (l *Logger) Info(args ...interface{}) {
-	Info(args...)
+func Info(args ...interface{}) {
+	log.Info(args...)
 }
 
 // Info prints in verbose mode to standart output. Format according to fmt.Println.
-func Info(args ...interface{}) {
-	if log.verbose {
+func (l *Logger) Info(args ...interface{}) {
+	if l.verbose {
 		fmt.Println(args...)
 	}
 }
 
 // Infof formatted prints in verbose mode to standart output. Format according to fmt.Printf.
-func (l *Logger) Infof(format string, args ...interface{}) {
-	Infof(format, args...)
+func Infof(format string, args ...interface{}) {
+	log.Infof(format, args...)
 }
 
 // Infof formatted prints in verbose mode to standart output. Format according to fmt.Printf.
-func Infof(format string, args ...interface{}) {
-	if log.verbose {
+func (l *Logger) Infof(format string, args ...interface{}) {
+	if l.verbose {
 		fmt.Printf(format, args...)
 	}
 }
