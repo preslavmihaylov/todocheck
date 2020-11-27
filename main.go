3c28f21c91f52f59d634c226ea2537dd1df1d155
preslavmihaylov <preslav@pmihaylov.com>
2020-11-27 21:15:05 +0200

    foobar

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ var version string
 // * Add a --closes option which indicates that an issue is to be closed as a result of a PR
 // * Add caching for task statuses
 func main() {
-	fmt.Println("FOOOO")
+	fmt.Println("FOOOOaa")
 	return
 	var basepath = flag.String("basepath", ".", "The path for the project to todocheck. Defaults to current directory")
 	var cfgPath = flag.String("config", "", "The project configuration file to use. Will use the one from the basepath if not specified")
