54774491fc5fcaeacc26974155f7d3da10bd4b97
Hristiyan Bonev <hristiyan.bonev@ocado.com>
2021-07-06 19:40:53 +0300

    Add support for closed Azure tasks

--- issuetracker/internal/azureboards/task.go
+++ issuetracker/internal/azureboards/task.go
@@ -4,13 +4,18 @@ import "github.com/preslavmihaylov/todocheck/issuetracker/taskstatus"
 
 // Task model
 type Task struct {
-	State string `json:"state"`
+	ID     int `json:"id"`
+	Fields struct {
+		State string `json:"System.State"`
+	}
 }
 
 // GetStatus of github task, based on underlying structure
 func (t *Task) GetStatus() taskstatus.TaskStatus {
-	switch t.State {
-	case "closed":
+	switch t.Fields.State {
+	case "To Do":
+		fallthrough
+	case "Done":
 		return taskstatus.Closed
 	default:
 		return taskstatus.Open
