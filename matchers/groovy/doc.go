725e16e6e7a680c40142c225794e85a5e8f0b030
eisandbar <eisandbar@gmail.com>
2022-08-11 10:25:20 -0400

    Fix lint issues
    Linter was complaining that files were not gofmt-ed with -s. Ran golangci-lint run --fix

--- matchers/groovy/doc.go
+++ matchers/groovy/doc.go
@@ -1,4 +1,4 @@
 // Package groovy contains a todo matcher & comments matcher for groovy comments.
 // Groovy comments, at their core, are standard C-like '//' and '/* */' comments.
-// However, there is also support for multi-line strings using """ and ''' string literals.
+// However, there is also support for multi-line strings using """ and ”' string literals.
 package groovy
